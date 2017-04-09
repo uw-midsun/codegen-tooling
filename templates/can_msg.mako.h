5171fa62149fcc0e35973cf6d70179fb3b1f638b
Karl <karlding@users.noreply.github.com>
2017-04-09 13:51:08 -0400

    Remove unused include

--- templates/can_msg.mako.h
+++ templates/can_msg.mako.h
@@ -1,7 +1,5 @@
 <%namespace name="helpers" file="/helpers/c.helpers.mako" /> \
 <% from data import NUM_CAN_MESSAGES, NUM_CAN_DEVICES, parse_can_device_enum, parse_can_message_enum %>
-#include <stdint.h>
-
 // For setting the CAN device 
 typedef enum {
   <% can_devices = parse_can_device_enum() %>
