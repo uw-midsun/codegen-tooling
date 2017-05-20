261a015253d211d6a71afd400d52745b74d2e864
karl ding <karlding@users.noreply.github.com>
2017-05-20 15:25:12 -0400

    ELEC-130: Add CAN messages (#4)
    * Add protobuf CAN messages
    * Move python files to codegen/
    * Move generated files to genfiles/

--- templates/can_msg.mako.ts
+++ templates/can_msg.mako.ts
@@ -1,15 +1,13 @@
 <%namespace name="helpers" file="/helpers/helpers.mako" /> \
 <% from data import NUM_CAN_MESSAGES, NUM_CAN_DEVICES, parse_can_device_enum, parse_can_message_enum %> \
-#pragma once
-
-// For setting the CAN device 
-typedef enum { 
+// For setting the CAN device
+export const enum CanDevice {
   <% can_devices = parse_can_device_enum() %> \
   ${helpers.generate_enum(can_devices, 'CAN_DEVICE')}
-} CanDevice;
+}
 
 // For setting the CAN message ID
-typedef enum {
+export const enum CanDevice {
   <% can_messages = parse_can_message_enum(options.filename) %> \
   ${helpers.generate_enum(can_messages, 'CAN_MESSAGE')}
-} CanMessage;
+}
