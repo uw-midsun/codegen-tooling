5efe904b0978875a76920c7cd0f5ee36c8efdcb6
ckitagawa <calderk2010@gmail.com>
2018-02-17 17:03:05 -0500

    ELEC-353: Fix format

--- templates/can_msg_defs.mako.h
+++ templates/can_msg_defs.mako.h
@@ -6,18 +6,14 @@
 
 #include "can_msg.h"
 
-#include <stdbool.h>
-
-#include "can_msg.h"
-
 // For setting the CAN device
 typedef enum {
-  <% can_devices = parse_can_device_enum() %>
-      ${ helpers.generate_enum(can_devices, 'SYSTEM_CAN_DEVICE') }
+  <% can_devices = parse_can_device_enum() %> \
+      ${helpers.generate_enum(can_devices, 'SYSTEM_CAN_DEVICE')}
 } SystemCanDevice;
 
 // For setting the CAN message ID
 typedef enum {
-  <% can_messages = parse_can_message_enum(options.filename) %>
-      ${ helpers.generate_enum(can_messages, 'SYSTEM_CAN_MESSAGE') }
+  <% can_messages = parse_can_message_enum(options.filename) %> \
+      ${helpers.generate_enum(can_messages, 'SYSTEM_CAN_MESSAGE')}
 } SystemCanMessage;
